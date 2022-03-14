87f2554b662623ef60c60d8d4a9634c5884be21b
laura bonnet <bonnet_laura@hotmail.fr>
2022-03-14 14:14:35 +0000

    'initialcommit'

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if 'git+' not in x]
+
+setup(name='find_your_inner_gamer',
+      version="1.0",
+      description="Project Description",
+      packages=find_packages(),
+      install_requires=requirements,
+      test_suite='tests',
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      scripts=['scripts/find_your_inner_gamer-run'],
+      zip_safe=False)
